55a0e03091d203612eb0f5190a48e243c691f45b
sqrt289 <7u.bayramov7@gmail.com>
2020-02-10 08:12:55 +0300

    commit

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -54,8 +54,8 @@ MIDDLEWARE = [
 ROOT_URLCONF = 'mysite.urls'
 
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '7u.bayramov7@gmail.com'
-EMAIL_HOST_PASSWORD = 'sSSFWUNcvfn4561csc'
+EMAIL_HOST_USER = '****'
+EMAIL_HOST_PASSWORD = '******'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
